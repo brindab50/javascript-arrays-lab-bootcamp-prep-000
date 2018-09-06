3e935475df4bba4725bbb3c1b0976be12f331e0e
brindab50@gmail.com behal <brindab50@gmail.com>
2018-09-06 17:58:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+var kittens = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendKitten(name) {
-  array.push(name);
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kitten.unshift(name);
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop(name);
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kitten.shift();
+  return kittens
+}
+function appendKitten(name){
+  [array, kittens];
   return kitten
+}
+function prepenedKitten(name) {
+  [kitten, ...array];
+  return array
+}
+function removeLastKitten() {
+  kitten.slice(0, kittens.length - 3);
+  return array
 }
